361f91d5a58f2ade5c3e26a6e4095c268273976e
sathyainfotech <84429523+sathyainfotech@users.noreply.github.com>
2022-11-29 22:41:30 +0900

    Update script.js

--- Bulb-On-Off/JS/script.js
+++ Bulb-On-Off/JS/script.js
@@ -1,6 +1,24 @@
+	var check=true;
+ 	document.querySelector('button').style.backgroundColor='#4CAF50';
 
-const light = document.querySelector("#bulb");
+	function myFunction(){
+		if(check){
+			check=false;
+			document.querySelector('#img').src='Image/lighton.png';
+			document.querySelector('button').innerHTML='OFF';
+			document.querySelector('button').style.backgroundColor='#f44336';					
+		}else{
+			check=true;
+			document.querySelector('button').innerHTML='ON';			
+			document.querySelector('button').style.backgroundColor='#4CAF50';
+			document.querySelector('#img').src='Image/lightoff.png';
+		}
+	}
 
-light.addEventListener("click", function(e){
-	e.target.classList.toggle("bulb-on");
-}); 
+// const light = document.querySelector('#bulb');
+
+// light.addEventListener('click', function(e){
+
+//     e.target.classList.toggle('bulb-on');
+
+// })
